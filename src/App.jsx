dc397a5ec48656608d2a5cfa2a810e4bc0f01f81
Umasri537 <gubbalaumasri4@gmail.com>
2025-09-25 13:08:34 +0530

    React Basic Todolist

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from"./mainarea/MainArea"
+
+function App() {
+ return (
+    <div className="border border-2 m-4 p-4 border-secondary">
+     <h1>TodoLIst Here:</h1>
+     <TodoList></TodoList>
+    </div>
+  )
+}
+
+export default App

b8931c83cfb15cc2b977e5314c0d80cf6b30a28a
Umasri537 <gubbalaumasri4@gmail.com>
2025-11-24 11:22:51 +0530

    Todolist

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TodoList from"./mainarea/MainArea"
 
 function App() {
  return (
-    <div className="border border-2 m-4 p-4 border-secondary">
+    <div className="border border-1 m-4 p-4 border-dark">
      <h1>TodoList:</h1>
      <TodoList></TodoList>
     </div>
